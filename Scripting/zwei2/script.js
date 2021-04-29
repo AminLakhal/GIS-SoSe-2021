70c9a9c183f8e3a012819d28c2d9099486bf71e3
AminLakhal <64310291+AminLakhal@users.noreply.github.com>
2021-04-29 08:50:04 +0200

    zwei2

--- Scripting/zwei2/script.js
+++ Scripting/zwei2/script.js
@@ -1,2 +1,10 @@
 "use strict";
+var Aufgabe1a;
+(function (Aufgabe1a) {
+    function min(_zahlen) {
+        let x = Math.min(..._zahlen);
+        console.log(x);
+    }
+    min([50, 75, 236, 83, 3478, 213, 2]);
+})(Aufgabe1a || (Aufgabe1a = {}));
 //# sourceMappingURL=script.js.map
---
+++ Scripting/zwei2/script.ts
@@ -0,0 +1,7 @@
+namespace Aufgabe1a {
+    function min(_zahlen:number[]) {
+       let x:number = Math.min(..._zahlen);
+       console.log(x);
+    }
+    min([50, 75,236,83,3478,213,2]);
+}
