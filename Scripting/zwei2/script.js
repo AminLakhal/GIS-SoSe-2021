bb6a9c4a9cf90d78daeb24175a44cd4813efea83
AminLakhal <64310291+AminLakhal@users.noreply.github.com>
2021-05-02 09:50:42 +0200

    2.2

--- Scripting/zwei2/script.js
+++ Scripting/zwei2/script.js
@@ -16,7 +16,6 @@ var Aufgabe1a;
 })(Aufgabe1a || (Aufgabe1a = {}));
 var Aufgabe1b;
 (function (Aufgabe1b) {
-    debugger;
     function isEven(_n) {
         if (_n == 0) {
             console.log("true");
@@ -30,11 +29,57 @@ var Aufgabe1b;
             return isEven(_n - 2);
         }
     }
-    isEven(-1);
+    isEven(50);
     /*
 
     50 = true
     75 = false
     -1 "Maximum call stack size exceeded" weil die funktion sich immer wieder selbst aufruft da n nicht 0 oder 1 werden kann */
 })(Aufgabe1b || (Aufgabe1b = {}));
+var Aufgabe1c;
+(function (Aufgabe1c) {
+    let s1 = { name: "Doe", vorname: "Jane", alter: 22, mNummer: 123456, sGang: "Mib", semester: 2 };
+    let s2 = { name: "Doe", vorname: "John", alter: 21, mNummer: 654321, sGang: "Mkb", semester: 1 };
+    let s3 = { name: "Gudien", vorname: "Stang", alter: 23, mNummer: 321654, sGang: "Omb", semester: 4 };
+    /*console.log(s1.alter);
+    console.log(s2.name);
+    console.log(s3.name);*/
+    let studenten = [s1, s2, s3, { name: "Dane", vorname: "Joe", alter: 20, mNummer: 654123, sGang: "Mib", semester: 2 }];
+    console.log(studenten[0].sGang, studenten[2].name, studenten[3].alter);
+    function showInfo(_info) {
+        console.log(_info.vorname, _info.name, _info.sGang, _info.semester);
+    }
+    showInfo(s1);
+    class newStudent {
+        constructor(_vorname, _name, _sGang, _semester, _alter, _mNummer) {
+            this.name = _name;
+            this.vorname = _vorname;
+            this.alter = _alter;
+            this.mNummer = _mNummer;
+            this.sGang = _sGang;
+            this.semester = _semester;
+        }
+        showInfo() {
+            console.log(this.vorname, this.name, this.sGang, this.semester, this.alter, this.mNummer);
+        }
+    }
+    let s = new newStudent("Jane", "Doe", "Mib", 4, 22, 123123);
+    s.showInfo();
+})(Aufgabe1c || (Aufgabe1c = {}));
+var Aufgabe2a;
+(function (Aufgabe2a) {
+    function backwards(..._array) {
+        for (let i = _array.length - 1; i >= 0; i--) {
+            console.log(_array[i]);
+        }
+    }
+    backwards(1, 2, 3, 4, 5, 6, 7, 8, 9);
+})(Aufgabe2a || (Aufgabe2a = {}));
+var Aufgabe2b;
+(function (Aufgabe2b) {
+    let array1 = ["apfel", "birne", "zitrone"];
+    let array2 = [1, 2, 3];
+    let mergedArray = [...array1, ...array2];
+    console.log(mergedArray);
+})(Aufgabe2b || (Aufgabe2b = {}));
 //# sourceMappingURL=script.js.map
--- Scripting/zwei2/script.ts
+++ Scripting/zwei2/script.ts
@@ -12,13 +12,12 @@ namespace Aufgabe1a {
         console.log(minvalue);
         return minvalue;
     }
-    min( 1, 50, 75, 236, 14, 5, 7, 83, 3478, 213, 2);
+    min(1, 50, 75, 236, 14, 5, 7, 83, 3478, 213, 2);
 }
 
 
 
 namespace Aufgabe1b {
-    debugger;
     function isEven(_n: number): boolean {
         if (_n == 0) {
             console.log("true");
@@ -34,7 +33,7 @@ namespace Aufgabe1b {
             return isEven(_n - 2);
         }
     }
-    isEven(-1);
+    isEven(50);
     /*
 
     50 = true
@@ -44,17 +43,78 @@ namespace Aufgabe1b {
 }
 namespace Aufgabe1c {
 
-    interface Studenten {
+    interface Student {
         name: string;
         vorname: string;
-        geburtsdatum: string;
         alter: number;
-        matrikelnummer: number;
-        studiengang: string:
+        mNummer: number;
+        sGang: string;
         semester: number;
-        email(mail: string): void;
+
     }
 
+    let s1: Student = { name: "Doe", vorname: "Jane", alter: 22, mNummer: 123456, sGang: "Mib", semester: 2 };
+    let s2: Student = { name: "Doe", vorname: "John", alter: 21, mNummer: 654321, sGang: "Mkb", semester: 1 };
+    let s3: Student = { name: "Gudien", vorname: "Stang", alter: 23, mNummer: 321654, sGang: "Omb", semester: 4 };
+    /*console.log(s1.alter);
+    console.log(s2.name);
+    console.log(s3.name);*/
 
+    let studenten: Student[] = [s1, s2, s3, { name: "Dane", vorname: "Joe", alter: 20, mNummer: 654123, sGang: "Mib", semester: 2 }];
+    console.log(studenten[0].sGang, studenten[2].name, studenten[3].alter);
+
+    function showInfo(_info: Student) {
+        console.log(_info.vorname, _info.name, _info.sGang, _info.semester);
+    }
+    showInfo(s1);
+
+
+    class newStudent {
+        name: string;
+        vorname: string;
+        alter: number;
+        mNummer: number;
+        sGang: string;
+        semester: number;
+
+        constructor(_vorname: string, _name: string, _sGang: string, _semester: number, _alter: number, _mNummer: number) {
+
+            this.name = _name;
+            this.vorname = _vorname;
+            this.alter = _alter;
+            this.mNummer = _mNummer;
+            this.sGang = _sGang;
+            this.semester = _semester;
+        }
+
+        showInfo(): void {
+            console.log(this.vorname, this.name, this.sGang, this.semester, this.alter, this.mNummer);
+        }
+    }
+
+    let s: newStudent = new newStudent("Jane", "Doe", "Mib", 4, 22, 123123);
+    s.showInfo();
+
+}
+
+namespace Aufgabe2a {
+
+    function backwards(..._array: number[]) {
+        for (let i: number = _array.length - 1; i >= 0; i--) {
+            console.log(_array[i]);
+        }
+
+    }
+    backwards(1, 2, 3, 4, 5, 6, 7, 8, 9);
+}
+
+namespace Aufgabe2b {
+
+    let array1:string[] = ["apfel", "birne", "zitrone"];
+    let array2:number[] = [1, 2, 3];
+
+    let mergedArray = [...array1, ...array2];
+
+    console.log(mergedArray);
 
 }
